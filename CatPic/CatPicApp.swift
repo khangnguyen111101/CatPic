a96658727cba8cb5f20cafbb40644f8bea51d508
khangnguyen111101 <khangnguyen111101@gmail.com>
2022-07-22 09:03:13 +0700

    Initial Commit

---
+++ CatPic/CatPicApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatPicApp.swift
+//  CatPic
+//
+//  Created by Khang Nguyen Bao on 22/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct CatPicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CatPic/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CatPic
+//
+//  Created by Khang Nguyen Bao on 22/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
